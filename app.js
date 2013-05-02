fbf53cffc8b2f87ae2419e1ed939513779393c10
aji <a.ji@daum.net>
2013-05-02 17:17:16 +0900

    underscore.js추가

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
 
+	var API_KEY = "f3330b2b08327bc2ed0806b5bc153de024c65e29";
+	var API_URL = "http://apis.daum.net/search/vclip?apikey={apikey}&q={query}&result=5&pageno={pageno}&output=json&callback=?";
+	var SEARCH_TYPE = ["블로그","카페","게시판"];
+	var SEARCH_API_URL = [
+		"http://apis.daum.net/search/blog",
+		"http://apis.daum.net/search/cafe",
+		"http://apis.daum.net/search/board"];
+
+	var searchTypeIndex = 0;
 
 
 	var searchQuery = function(){
 
-		console.log(1111)
+		
 
 		return false;
 	};
+
+
 
 	$('.search-form').on("submit", searchQuery);
 
